2293634ea9bd3c8610cc389fb069a74fcf830f1f
Bichitra Gautam <bichitragautam25@gmail.com>
2024-05-15 20:24:44 +0545

    feat: Add Google sign-in option to UserAuthForm page

--- blogging website - frontend/src/pages/userAuthForm.page.jsx
+++ blogging website - frontend/src/pages/userAuthForm.page.jsx
@@ -1,4 +1,5 @@
 import InputBox from "../components/input.component";
+import googleIcon from "../imgs/google.png";
 
 const UserAuthForm = ({ type }) => {
     return (
@@ -30,6 +31,24 @@ const UserAuthForm = ({ type }) => {
                     placeholder="Enter your Password"
                     icon="fi-rr-key"
                 />
+
+                <button className="btn-dark center mt-14"
+                    type="submit"
+                >
+                    {type === "sign-in" ? "Sign In" : "Sign Up"}
+                </button>
+
+                <div className="relative w-full items-center gap-2 my-10 opacity-10 uppercase text-black font-bold">
+                    <hr className="w-1/2 border-black" />
+                    <p>or</p>
+                    <hr className="w-1/2 border-black" />
+
+                </div>
+
+                <button className="btn-dark flex items-center justify-center gap-4 w-[90%] center">
+                    <img src={googleIcon} className="w-5 h-5" />
+                    Continue With Google
+                </button>
 
 
             </form>
