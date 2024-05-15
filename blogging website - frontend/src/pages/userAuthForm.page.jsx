6b5435be2970b8b76ba708c19ad7af425357e67e
Bichitra Gautam <bichitragautam25@gmail.com>
2024-05-15 20:02:34 +0545

    Update UserAuthForm component to display different text based on authentication type

--- blogging website - frontend/src/pages/userAuthForm.page.jsx
+++ blogging website - frontend/src/pages/userAuthForm.page.jsx
@@ -1,10 +1,37 @@
+import InputBox from "../components/input.component";
+
 const UserAuthForm = ({ type }) => {
     return (
         <section className="h-cover flex items-center justify-center">
             <form className="w-[80%] max-w-[400px]">
                 <h1 className="text-4xl capitalize text-center mb-24">
-                    {type}
+                    {type === "sign-in" ? "Welcome Back" : "Join Us Today"}
                 </h1>
+                {
+                    type != "sign-in" ?
+                        <InputBox
+                            name="fullname"
+                            type="text"
+                            placeholder="Enter your Full Name"
+                            icon="fi-rr-user"
+                        /> : ""
+                }
+
+                <InputBox
+                    name="email"
+                    type="email"
+                    placeholder="Enter your Email Address"
+                    icon="fi-rr-envelope"
+                />
+
+                <InputBox
+                    name="password"
+                    type="password"
+                    placeholder="Enter your Password"
+                    icon="fi-rr-key"
+                />
+
+
             </form>
         </section>
     )
