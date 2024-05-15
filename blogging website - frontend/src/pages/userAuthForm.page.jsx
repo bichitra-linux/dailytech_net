6bbdc1ac5bfac0a9932b2cf99d5b4ebc3429abcb
Bichitra Gautam <bichitragautam25@gmail.com>
2024-05-15 20:40:34 +0545

    refactor: Update UserAuthForm page layout and add AnimationWrapper component

--- blogging website - frontend/src/pages/userAuthForm.page.jsx
+++ blogging website - frontend/src/pages/userAuthForm.page.jsx
@@ -1,69 +1,72 @@
 import { Link } from "react-router-dom";
 import InputBox from "../components/input.component";
 import googleIcon from "../imgs/google.png";
+import AnimationWrapper from "../common/page-animation";
 
 const UserAuthForm = ({ type }) => {
     return (
-        <section className="h-cover flex items-center justify-center">
-            <form className="w-[80%] max-w-[400px]">
-                <h1 className="text-4xl capitalize text-center mb-24">
-                    {type === "sign-in" ? "Welcome Back" : "Join Us Today"}
-                </h1>
-                {
-                    type != "sign-in" ?
-                        <InputBox
-                            name="fullname"
-                            type="text"
-                            placeholder="Enter your Full Name"
-                            icon="fi-rr-user"
-                        /> : ""
-                }
+        <AnimationWrapper>
+            <section className="h-cover flex items-center justify-center">
+                <form className="w-[80%] max-w-[400px]">
+                    <h1 className="text-4xl capitalize text-center mb-24">
+                        {type === "sign-in" ? "Welcome Back" : "Join Us Today"}
+                    </h1>
+                    {
+                        type != "sign-in" ?
+                            <InputBox
+                                name="fullname"
+                                type="text"
+                                placeholder="Enter your Full Name"
+                                icon="fi-rr-user"
+                            /> : ""
+                    }
 
-                <InputBox
-                    name="email"
-                    type="email"
-                    placeholder="Enter your Email Address"
-                    icon="fi-rr-envelope"
-                />
+                    <InputBox
+                        name="email"
+                        type="email"
+                        placeholder="Enter your Email Address"
+                        icon="fi-rr-envelope"
+                    />
 
-                <InputBox
-                    name="password"
-                    type="password"
-                    placeholder="Enter your Password"
-                    icon="fi-rr-key"
-                />
+                    <InputBox
+                        name="password"
+                        type="password"
+                        placeholder="Enter your Password"
+                        icon="fi-rr-key"
+                    />
 
-                <button className="btn-dark center mt-14"
-                    type="submit"
-                >
-                    {type === "sign-in" ? "Sign In" : "Sign Up"}
-                </button>
+                    <button className="btn-dark center mt-14"
+                        type="submit"
+                    >
+                        {type === "sign-in" ? "Sign In" : "Sign Up"}
+                    </button>
 
-                <div className="relative flex w-full items-center gap-2 my-10 opacity-10 uppercase text-black font-bold">
-                    <hr className="w-1/2 border-black" />
-                    <p>or</p>
-                    <hr className="w-1/2 border-black" />
+                    <div className="relative flex w-full items-center gap-2 my-10 opacity-10 uppercase text-black font-bold">
+                        <hr className="w-1/2 border-black" />
+                        <p>or</p>
+                        <hr className="w-1/2 border-black" />
 
-                </div>
+                    </div>
 
-                <button className="btn-dark flex items-center justify-center gap-4 w-[90%] center">
-                    <img src={googleIcon} className="w-5 h-5" />
-                    Continue With Google
-                </button>
+                    <button className="btn-dark flex items-center justify-center gap-4 w-[90%] center">
+                        <img src={googleIcon} className="w-5 h-5" />
+                        Continue With Google
+                    </button>
 
-                {
-                    type === "sign-in" ?
-                        <p className="text-center text-black text-xl mt-4">
-                            Don't have an account? <Link to="/signup" className="underline text-blue ml-1">Sign Up</Link>
-                        </p> :
-                        <p className="text-center text-black text-xl mt-4">
-                            Already have an account? <Link to="/signin" className="underline text-blue ml-1">Sign In</Link>
-                        </p>
-                }
+                    {
+                        type === "sign-in" ?
+                            <p className="text-center text-black text-xl mt-4">
+                                Don't have an account? <Link to="/signup" className="underline text-blue ml-1">Sign Up</Link>
+                            </p> :
+                            <p className="text-center text-black text-xl mt-4">
+                                Already have an account? <Link to="/signin" className="underline text-blue ml-1">Sign In</Link>
+                            </p>
+                    }
 
 
-            </form>
-        </section>
+                </form>
+            </section>
+        </AnimationWrapper>
     )
 }
 
