96cdfa538be5a19e1ce0df6e55a0d98c9b8b01fc
Bichitra Gautam <bichitragautam25@gmail.com>
2024-05-16 20:40:11 +0545

    refactor: Update AnimationWrapper component to accept initial and animate props

--- blogging website - frontend/src/common/page-animation.jsx
+++ blogging website - frontend/src/common/page-animation.jsx
@@ -1,9 +1,10 @@
 import { AnimatePresence, motion } from "framer-motion"
 
-const AnimationWrapper = ({ children, initial }) => {
+const AnimationWrapper = ({ children, initial = { opacity: 0 }, animate = { opacity: 1 } }) => {
     return (
         <motion.div
             initial={initial}
+            animate={animate}
         >
             { children }
         </motion.div>
