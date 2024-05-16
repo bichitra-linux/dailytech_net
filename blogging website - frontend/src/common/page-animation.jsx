62efdf50aa3d85c72cd1ac6a3001658331602514
Bichitra Gautam <bichitragautam25@gmail.com>
2024-05-16 20:45:49 +0545

    refactor: Update AnimationWrapper component to include key prop

--- blogging website - frontend/src/common/page-animation.jsx
+++ blogging website - frontend/src/common/page-animation.jsx
@@ -1,8 +1,9 @@
 import { AnimatePresence, motion } from "framer-motion"
 
-const AnimationWrapper = ({ children, initial = { opacity: 0 }, animate = { opacity: 1 }, transition = { duration: 1.2 } }) => {
+const AnimationWrapper = ({ children, keyValue, initial = { opacity: 0 }, animate = { opacity: 1 }, transition = { duration: 1.2 } }) => {
     return (
         <motion.div
+            key={keyValue}
             initial={initial}
             animate={animate}
             transition={transition}
