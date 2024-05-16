b37c604abbf9a28619bf2988c8fe7ab2452db16e
Bichitra Gautam <bichitragautam25@gmail.com>
2024-05-16 21:13:41 +0545

    refactor: Update server.js to use dotenv for environment variable configuration

--- server/server.js
+++ server/server.js
@@ -1,10 +1,17 @@
 import express from "express";
 import mongoose from "mongoose";
+import 'dotenv/config' ;
 
 const server = express();
 
 // Connect to MongoDB
-mongoose.connect()
+mongoose.connect(process.env.DB_LOCATION, {
+    autoIndex: true
+})
+
+server.post("/signup", (req, res) => {
+    res.json(req.body)
+})
 
 let PORT = "3000";
 
