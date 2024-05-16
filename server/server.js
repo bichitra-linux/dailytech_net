3848e41d9770106aa201637bc99e721d462bed21
Bichitra Gautam <bichitragautam25@gmail.com>
2024-05-16 22:08:17 +0545

    refactor: Improve server.js with enhanced validation in signup endpoint

--- server/server.js
+++ server/server.js
@@ -7,8 +7,8 @@ server.use(express.json());
 
 let PORT = "3000";
 
-let emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
-let passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+let emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;    // regex for e-mail
+let passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;   // regex for password
 
 
 // Connect to MongoDB
@@ -16,19 +16,25 @@ mongoose.connect(process.env.DB_LOCATION, {
     autoIndex: true
 })
 
-// Define a schema
+// Check if the connection is successful 
 server.post("/signup", (req, res) => {
     
     const { email, password, fullname } = req.body;
 
     if (fullname.length < 3 ){
-        return res.status(403).json({error: "Fullname must be at least 3 characters long"})  
+        return res.status(403).json({error: "Full Name must be at least 3 characters long"})  
     }
     if (!email.length){
         return res.status(403).json({error: "Email is required"})
     }
+    if (!emailRegex.test(email)){
+        return res.status(403).json({error: "Invalid email"})
+    }
     if (password.length < 8){
         return res.status(403).json({error: "Password must be at least 8 characters long"})
+    }
+    if (!passwordRegex.test(password)){
+        return res.status(403).json({error: "Password must contain at least one uppercase letter, one lowercase letter, one number and one special character"})
     }
     return res.status(200).json({"status": "ok"})
 
@@ -37,10 +43,22 @@ server.post("/signup", (req, res) => {
 })
 
 server.post("/signin", (req, res) => {
-    res.json(req.body)
+    const { email, password } = req.body;
+
+    if (!email.length){
+        return res.status(403).json({error: "Email is required"})
+    }
+    if (!emailRegex.test(email)){
+        return res.status(403).json({error: "Invalid email"})
+    }
+    if (password.length < 8){
+        return res.status(403).json({error: "Password must be at least 8 characters long"})
+    }
+    if (!passwordRegex.test(password)){
+        return res.status(403).json({error: "Password must contain at least one uppercase letter, one lowercase letter, one number and one special character"})
+    }
+    return res.status(200).json({"status": "ok"})
 })
-
-
 
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
