ca03710d798c4663630d6bc7a04e5d6f5146ed86
Bichitra Gautam <bichitragautam25@gmail.com>
2024-05-16 20:49:45 +0545

    refactor: Update AnimationWrapper component to include className prop

--- blogging website - frontend/src/common/page-animation.jsx
+++ blogging website - frontend/src/common/page-animation.jsx
@@ -1,15 +1,18 @@
 import { AnimatePresence, motion } from "framer-motion"
 
-const AnimationWrapper = ({ children, keyValue, initial = { opacity: 0 }, animate = { opacity: 1 }, transition = { duration: 1.2 } }) => {
+const AnimationWrapper = ({ children, keyValue, initial = { opacity: 0 }, animate = { opacity: 1 }, transition = { duration: 1.1 }, className }) => {
     return (
+        <AnimatePresence>
         <motion.div
             key={keyValue}
             initial={initial}
             animate={animate}
             transition={transition}
+            className={className}
         >
             { children }
         </motion.div>
+        </AnimatePresence>
     )
 }
 
