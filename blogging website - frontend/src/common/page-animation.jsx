016227d8757535d308e17eeff7f9aa7189749352
Bichitra Gautam <bichitragautam25@gmail.com>
2024-05-16 20:44:02 +0545

    refactor: Update AnimationWrapper component to include transition prop

--- blogging website - frontend/src/common/page-animation.jsx
+++ blogging website - frontend/src/common/page-animation.jsx
@@ -1,10 +1,11 @@
 import { AnimatePresence, motion } from "framer-motion"
 
-const AnimationWrapper = ({ children, initial = { opacity: 0 }, animate = { opacity: 1 } }) => {
+const AnimationWrapper = ({ children, initial = { opacity: 0 }, animate = { opacity: 1 }, transition = { duration: 1.2 } }) => {
     return (
         <motion.div
             initial={initial}
             animate={animate}
+            transition={transition}
         >
             { children }
         </motion.div>
