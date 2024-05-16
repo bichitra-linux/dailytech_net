be7052a81b86c4615113184041908ab2a5f8eb28
Bichitra Gautam <bichitragautam25@gmail.com>
2024-05-16 22:33:09 +0545

    refactor: Improve server.js with enhanced validation in signup endpoint

--- server/server.js
+++ server/server.js
@@ -1,7 +1,10 @@
 import express from "express";
 import mongoose from "mongoose";
 import bcrypt from "bcrypt";
-import 'dotenv/config' ;
+import 'dotenv/config';
+
+//schema imports
+import User from "./Schema/User.js";
 
 const server = express();
 server.use(express.json());
@@ -19,58 +22,80 @@ mongoose.connect(process.env.DB_LOCATION, {
 
 // Check if the connection is successful 
 server.post("/signup", (req, res) => {
-    
+
     const { email, password, fullname } = req.body;
 
-    if (fullname.length < 3 ){
-        return res.status(403).json({error: "Full Name must be at least 3 characters long"})  
+    if (fullname.length < 3) {
+        return res.status(403).json({ error: "Full Name must be at least 3 characters long" })
     }
-    if (!email.length){
-        return res.status(403).json({error: "Email is required"})
+    if (!email.length) {
+        return res.status(403).json({ error: "Email is required" })
     }
-    if (!emailRegex.test(email)){
-        return res.status(403).json({error: "Invalid email"})
+    if (!emailRegex.test(email)) {
+        return res.status(403).json({ error: "Invalid email" })
     }
-    if (password.length < 8){
-        return res.status(403).json({error: "Password must be at least 8 characters long"})
+    if (password.length < 8) {
+        return res.status(403).json({ error: "Password must be at least 8 characters long" })
     }
-    if (!passwordRegex.test(password)){
-        return res.status(403).json({error: "Password must contain at least one uppercase letter, one lowercase letter, one number and one special character"})
+    if (!passwordRegex.test(password)) {
+        return res.status(403).json({ error: "Password must contain at least one uppercase letter, one lowercase letter, one number and one special character" })
     }
 
     bcrypt.hash(
         password,
         10,
         (err, hash) => {
+
+            let username = email.split(
+                "@"
+            )[0];
+
+            let user = new User({
+                personal_info: {
+                    fullname: fullname,
+                    email: email,
+                    password: hash,
+                    username: username
+                }
+            })
+
+            user.save().then((u) => {
+                return res.status(200).json({
+                    "status": "ok",
+                    "user": u
+                })
+            });
+
+
             if (err) {
-                return res.status(500).json({error: "Internal server error"})
+                return res.status(500).json({ error: "Internal server error" })
             }
-            console.log(hash)
+            
         }
     )
 
-    return res.status(200).json({"status": "ok"})
+    //return res.status(200).json({ "status": "ok" })
 
-    
+
 
 })
 
 server.post("/signin", (req, res) => {
     const { email, password } = req.body;
 
-    if (!email.length){
-        return res.status(403).json({error: "Email is required"})
+    if (!email.length) {
+        return res.status(403).json({ error: "Email is required" })
     }
-    if (!emailRegex.test(email)){
-        return res.status(403).json({error: "Invalid email"})
+    if (!emailRegex.test(email)) {
+        return res.status(403).json({ error: "Invalid email" })
     }
-    if (password.length < 8){
-        return res.status(403).json({error: "Password must be at least 8 characters long"})
+    if (password.length < 8) {
+        return res.status(403).json({ error: "Password must be at least 8 characters long" })
     }
-    if (!passwordRegex.test(password)){
-        return res.status(403).json({error: "Password must contain at least one uppercase letter, one lowercase letter, one number and one special character"})
+    if (!passwordRegex.test(password)) {
+        return res.status(403).json({ error: "Password must contain at least one uppercase letter, one lowercase letter, one number and one special character" })
     }
-    return res.status(200).json({"status": "ok"})
+    return res.status(200).json({ "status": "ok" })
 })
 
 server.listen(PORT, () => {
