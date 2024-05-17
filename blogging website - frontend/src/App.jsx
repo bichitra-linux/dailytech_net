fbfba9e545040892dfa0be77e1292f9dd5a35af1
Bichitra Gautam <bichitragautam25@gmail.com>
2024-05-17 21:04:00 +0545

    refactor: Update App.jsx to include UserContext provider

--- blogging website - frontend/src/App.jsx
+++ blogging website - frontend/src/App.jsx
@@ -1,15 +1,23 @@
 import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/navbar.component";
 import UserAuthForm from "./pages/userAuthForm.page";
+import { createContext, useState } from "react";
+
+export const UserContext = createContext({})
 
 const App = () => {
+
+    const [userAuth, setuserAuth] = useState();
+
     return (
-        <Routes>
-            <Route path="/" element={<Navbar />}>
-                <Route path="/signin" element={<UserAuthForm type="sign-in" />} />
-                <Route path="/signup" element={<UserAuthForm type="sign-up" />} />
-            </Route>
-        </Routes>
+        <UserContext.Provider value={{userAuth, setuserAuth}}>
+            <Routes>
+                <Route path="/" element={<Navbar />}>
+                    <Route path="/signin" element={<UserAuthForm type="sign-in" />} />
+                    <Route path="/signup" element={<UserAuthForm type="sign-up" />} />
+                </Route>
+            </Routes>
+        </UserContext.Provider>
     )
 }
 
