0d9bb354eeb7b9da133cd59c4505055ac4c543c5
Bichitra Gautam <bichitragautam25@gmail.com>
2024-08-25 14:01:38 +0545

    refactor: Update navbar component to remove empty span tag

--- blogging website - frontend/src/components/navbar.component.jsx
+++ blogging website - frontend/src/components/navbar.component.jsx
@@ -55,7 +55,7 @@ const Navbar = () => {
                     {
                         access_token ? (
                             <>
-                                <span>User is Logged In</span>
+                                
                                 <Link to={"/dashboard/notification"} >
                                     <button className="w-12 h-12 rounded-full bg-grey relative hover:bg-black/10">
                                     <i className="fi fi-rr-bell text-2xl block mt-1"></i>
