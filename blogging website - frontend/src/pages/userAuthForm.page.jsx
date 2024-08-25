0a9a11dd82d2e9ebebe0c3e3f75fe15c846ab510
Bichitra Gautam <bichitragautam25@gmail.com>
2024-08-25 12:36:21 +0545

    refactor: Update userAuthForm page to improve user authentication process

--- blogging website - frontend/src/pages/userAuthForm.page.jsx
+++ blogging website - frontend/src/pages/userAuthForm.page.jsx
@@ -75,71 +75,73 @@ const UserAuthForm = ({ type }) => {
     }
 
     return (
-        <AnimationWrapper keyValue={type}>
-            <section className="h-cover flex items-center justify-center">
-                <Toaster />
-                <form id="formElement" className="w-[80%] max-w-[400px]">
-                    <h1 className="text-4xl capitalize text-center mb-24">
-                        {type === "sign-in" ? "Welcome Back" : "Join Us Today"}
-                    </h1>
-                    {
-                        type != "sign-in" ?
+        access_token ? (
+            <Navigate to="/" />
+        ) : (
+            <>
+                <AnimationWrapper keyValue={type}>
+                    <section className="h-cover flex items-center justify-center">
+                        <Toaster />
+                        <form id="formElement" className="w-[80%] max-w-[400px]">
+                            <h1 className="text-4xl capitalize text-center mb-24">
+                                {type === "sign-in" ? "Welcome Back" : "Join Us Today"}
+                            </h1>
+                            {type !== "sign-in" ? (
+                                <InputBox
+                                    name="fullname"
+                                    type="text"
+                                    placeholder="Enter your Full Name"
+                                    icon="fi-rr-user"
+                                />
+                            ) : null}
+
                             <InputBox
-                                name="fullname"
-                                type="text"
-                                placeholder="Enter your Full Name"
-                                icon="fi-rr-user"
-                            /> : ""
-                    }
+                                name="email"
+                                type="email"
+                                placeholder="Enter your Email Address"
+                                icon="fi-rr-envelope"
+                            />
 
-                    <InputBox
-                        name="email"
-                        type="email"
-                        placeholder="Enter your Email Address"
-                        icon="fi-rr-envelope"
-                    />
+                            <InputBox
+                                name="password"
+                                type="password"
+                                placeholder="Enter your Password"
+                                icon="fi-rr-key"
+                            />
 
-                    <InputBox
-                        name="password"
-                        type="password"
-                        placeholder="Enter your Password"
-                        icon="fi-rr-key"
-                    />
+                            <button className="btn-dark center mt-14"
+                                type="submit"
+                                onClick={handleSubmit}
+                            >
+                                {type === "sign-in" ? "Sign In" : "Sign Up"}
+                            </button>
 
-                    <button className="btn-dark center mt-14"
-                        type="submit"
-                        onClick={handleSubmit}
-                    >
-                        {type === "sign-in" ? "Sign In" : "Sign Up"}
-                    </button>
+                            <div className="relative flex w-full items-center gap-2 my-10 opacity-10 uppercase text-black font-bold">
+                                <hr className="w-1/2 border-black" />
+                                <p>or</p>
+                                <hr className="w-1/2 border-black" />
+                            </div>
 
-                    <div className="relative flex w-full items-center gap-2 my-10 opacity-10 uppercase text-black font-bold">
-                        <hr className="w-1/2 border-black" />
-                        <p>or</p>
-                        <hr className="w-1/2 border-black" />
+                            <button className="btn-dark flex items-center justify-center gap-4 w-[90%] center">
+                                <img src={googleIcon} className="w-5 h-5" />
+                                Continue With Google
+                            </button>
 
-                    </div>
-
-                    <button className="btn-dark flex items-center justify-center gap-4 w-[90%] center">
-                        <img src={googleIcon} className="w-5 h-5" />
-                        Continue With Google
-                    </button>
-
-                    {
-                        type === "sign-in" ?
-                            <p className="text-center text-black text-xl mt-4">
-                                Don't have an account? <Link to="/signup" className="underline text-blue ml-1">Sign Up</Link>
-                            </p> :
-                            <p className="text-center text-black text-xl mt-4">
-                                Already have an account? <Link to="/signin" className="underline text-blue ml-1">Sign In</Link>
-                            </p>
-                    }
-
-
-                </form>
-            </section>
-        </AnimationWrapper>
-    )
+                            {type === "sign-in" ? (
+                                <p className="text-center text-black text-xl mt-4">
+                                    Don't have an account? <Link to="/signup" className="underline text-blue ml-1">Sign Up</Link>
+                                </p>
+                            ) : (
+                                <p className="text-center text-black text-xl mt-4">
+                                    Already have an account? <Link to="/signin" className="underline text-blue ml-1">Sign In</Link>
+                                </p>
+                            )}
+                        </form>
+                    </section>
+                </AnimationWrapper>
+            </>
+        )
+    );
 }
 
 export default UserAuthForm;
